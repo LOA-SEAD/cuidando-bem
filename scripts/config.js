9575803a6cca83730ce953d002b9724c144c32a6
Otho <marcelolotufo@hotmail.com>
2014-06-30 17:10:07 -0300

    Update files structure

--- scripts/config.js
+++ scripts/config.js
@@ -2,16 +2,9 @@
     This file is required by require.js to work properly. It configures all file paths so it is easier
 to import them inside each module.
 
-    Date: 30/06/2014
-    Author: Otho - Marcelo Lopes Lotufo;
-
-    Last Revision: (date)
-    Last Revisor: (name)
-
-
 */
 
-//Configure all files paths, so inside each module the files adress are shorter
+//Configure all files paths, so inside each module the files address are shorter
 require.config({
     baseUrl: 'scripts/',
     paths: {        
@@ -21,7 +14,7 @@ require.config({
     }
 });
 //Init the app when everything is ready
-require( ["jquery", "mainController"], function($, mainController, test) 
+require( ["jquery"], function($) 
 {
     $('document').ready(function()
     {
---
+++ scripts/model/gameConfig.js
@@ -0,0 +1,5 @@
+/*
+	This module has every wrold variable from each game level so it can be easily loaded inside the game.
+	New levels can easily be made by adding new game levels.
+	
+*/
