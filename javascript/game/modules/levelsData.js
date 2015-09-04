d4cfa6b6b229f571cd1cf6fd28ff366448bbefa8
0tho <marcelolotufo@hotmail.com>
2015-09-04 10:50:22 -0300

    Update

---
+++ javascript/game/modules/levelsData.js
@@ -0,0 +1,1 @@
+define([],function(){function n(t,n){e[n]!=null,e[n]=t}function r(){return e[t]}function i(){return t}function s(e){t=e}var e={},t=0;return{registerLevel:n,getCurrentLevel:r,getCurrentLevelId:i,setCurrentLevel:s}});
---
+++ javascript/game/scripts/gameConfig.js
@@ -0,0 +1,1 @@
+define([],function(){var e="./game/scripts/levels/",t=["Tutorial","Fase1","Fase2","Fase8"],n;for(n=0;n<t.length;n++)require([e+t[n]])});
