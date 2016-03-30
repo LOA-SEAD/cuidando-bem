ea5b8b07bfdc9c6bb74fed05cbd55e0f331c8c91
Otho <marcelo.lopes.lotufo@gmail.com>
2016-03-30 14:34:10 -0300

    Configurate grunt to build game using electron
    With electron the game can run on multiple dekstop plataforms
    The game is configured to build to all plataforms

---
+++ tasks/options/electron.js
@@ -0,0 +1,17 @@
+// connect
+"use strict";
+module.exports = {
+    options: {
+
+    },
+
+    publish: {
+        options: {
+            name: "Cuidando Bem",
+            dir: "prod",
+            out: "dist",
+            platform: "all",
+            arch: "all"
+        }
+    }
+};
