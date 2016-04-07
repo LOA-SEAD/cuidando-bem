28ed5c48208a27bcd0e28f5022bf38c1588dc951
Otho <marcelo.lopes.lotufo@gmail.com>
2016-04-07 02:36:30 -0300

    Enhance configuration screen

--- src/javascript/menu/screenConfigurationController.js
+++ src/javascript/menu/screenConfigurationController.js
@@ -6,9 +6,17 @@
  *
  * @author Otho - Marcelo Lopes Lotufo
  */
-define([ "Stage" ], function( Stage ) {
+define([ "Stage", "Storage" ], function( Stage, Storage ) {
 
     var Player = require("Player");
+
+    var sfxMuted = Storage.isSfxMuted();
+    var sfxVolume = Storage.getSfxVolume();
+    var musicMuted = Storage.isMusicMuted();
+    var musicVolume = Storage.getMusicVolume();
+
+    var sfxVolumeBeforeMute = Storage.getSfxVolume();
+    var musicVolumeBeforeMute = Storage.getMusicVolume();
 
     /**
      * This method is called when the screen Configuration is loaded
@@ -23,15 +31,70 @@ define([ "Stage" ], function( Stage ) {
             Player.play( Player.audios.sfx.selecionarMenu );
         });
 
-        $("#muteSoundButton").click(function() {
-            Player.mute();
-        });
-
         $(".backButton").click(function() {
             Stage.changeScreen( 0 );
         });
 
-        $(".slider").slider();
+        $(".slider.sfx").slider({
+            value: sfxVolume * 100,
+            max: 100
+        });
+
+        $(".slider.music").slider({
+            value: musicVolume * 100,
+            max: 100
+        });
+
+        if ( sfxMuted ) {
+            $(".mute.sfx").addClass( "desactive" );
+        }
+
+        if ( musicMuted ) {
+            $(".mute.music").addClass( "desactive" );
+        }
+
+        $(".slider.sfx").on( "slide", function( event, ui ) {
+            Storage.setSfxVolume( ui.value / 100 );
+            Player.setVolumeToCategory( Player.audios.sfx, ui.value / 100 );
+            sfxVolume = ui.value / 100;
+        });
+
+        $(".slider.music").on( "slide", function( event, ui ) {
+            Storage.setMusicVolume( ui.value / 100 );
+            Player.setVolumeToCategory( Player.audios.musics, ui.value / 100 );
+            musicVolume = ui.value / 100;
+        });
+
+         $(".mute.sfx").on( "click", function( event ) {
+            if ( !sfxMuted ) {
+                $( this ).addClass( "desactive" );
+                sfxVolumeBeforeMute = sfxVolume;
+                Player.setVolumeToCategory( Player.audios.sfx, 0 );
+                sfxVolume = 0;
+            } else {
+                $( this ).removeClass( "desactive" );
+                Player.setVolumeToCategory( Player.audios.sfx, sfxVolumeBeforeMute );
+                sfxVolume = sfxVolumeBeforeMute;
+            }
+            Storage.toggleSfxMute();
+            sfxMuted = !sfxMuted;
+        });
+
+        $(".mute.music").on( "click", function( event ) {
+            if ( !musicMuted ) {
+                $( this ).addClass( "desactive" );
+                musicVolumeBeforeMute = musicVolume;
+                Player.setVolumeToCategory( Player.audios.musics, 0 );
+                musicVolume = 0;
+            } else {
+                $( this ).removeClass( "desactive" );
+                Player.setVolumeToCategory( Player.audios.musics, musicVolumeBeforeMute );
+                musicVolume = musicVolumeBeforeMute;
+            }
+            Storage.toggleMusicMute();
+
+            musicMuted = !musicMuted;
+        });
     }
 
     /**
