a77a24b4377535465bf781c6aabcc13313e9b95f
Breno <brenocalixtoas@gmail.com>
2016-11-25 10:36:08 -0200

    disabled fichas inputs, new css info baloon

--- src/javascript/game/scripts/levels/fase5.js
+++ src/javascript/game/scripts/levels/fase5.js
@@ -122,6 +122,7 @@ define([ "levelsData", "Scene", "Action", "Level", "Dialog", "InteractiveObject"
 
     corredor = lib.scenes.corredor.getClone()
       .onLoad(function() {
+
         
         core.openCommandBar();
         core.setActionVisible("btn-ir_recepcao", true );
