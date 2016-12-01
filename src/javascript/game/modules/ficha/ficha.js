2f87a15fa4831fd02909d7dd3b5bfece5eef8ead
Breno <brenocalixtoas@gmail.com>
2016-12-01 14:07:18 -0200

    ficha verification - red on mistake

--- src/javascript/game/modules/ficha/ficha.js
+++ src/javascript/game/modules/ficha/ficha.js
@@ -77,6 +77,31 @@ define([ "text!../html/ficha/ficha.html" ], function( html ) {
     $( inVolume2 ).on("keyup paste change", function() {
       $( inVolume1 ).val( $( this ).val() );
     });
+      
+      
+      
+      var Hourmask = "HJ:MK",
+    pattern = {
+        'translation': {
+            'H': {
+                pattern: /[0-2]/
+            },
+            'J': {
+                pattern: /[0-9]/
+            },
+            'M': {
+                pattern: /[0-5]/
+            },
+            'K': {
+                pattern: /[0-9]/
+            },
+        }
+    };
+
+
+      
+      
+      
 
     $( inData ).mask("00/00/0000");
     $( inLeito ).mask("00");
@@ -85,8 +110,8 @@ define([ "text!../html/ficha/ficha.html" ], function( html ) {
     $( inDuracao ).mask("00");
     $( inGotas ).mask("00,00");
     $( inGotasAprox ).mask("000");
-    $( inIni ).mask("00:00");
-    $( inTer ).mask("00:00");
+    $( inIni ).mask(Hourmask,pattern);
+    $( inTer ).mask(Hourmask,pattern);
     $( inHorario ).mask("00:00");
   }
 
@@ -94,16 +119,6 @@ define([ "text!../html/ficha/ficha.html" ], function( html ) {
     if ( typeof _state === "undefined" || typeof _state === "undefined") {
       throw new Error("You must define a state to open ficha and the level");
     }
-      
-    /*   var inDataVal = $( inData ).val().split("/");
-      
-    inDataVal[ 0 ] == today.getDate() 
-    inDataVal[ 1 ] == today.getMonth() 
-    inDataVal[ 2 ] == today.getMonth() 
- 
-    if ( inDataVal[ 2 ] != today.getYear() + 1900 ) {
-      return false;
-    }*/
 
     state = _state;
     var _data = new Date();
@@ -145,24 +160,15 @@ define([ "text!../html/ficha/ficha.html" ], function( html ) {
   function close() {
     $( divSelector ).hide();
   }
+    
 
   function isDataValid() {
     // Geral
     // data - Verificar em relação ao dia mes e ano atual
-    var today = new Date();
-    var horas = today.getHours();
-    var minutos = today.getMinutes();
+   
 
-    var inDataVal = $( inData ).val().split("/");
-    if ( inDataVal[ 0 ] != today.getDate() ) {
-      return false;
-    }
-    if ( inDataVal[ 1 ] != today.getMonth() + 1 ) {
-      return false;
-    }
-    if ( inDataVal[ 2 ] != today.getYear() + 1900 ) {
-      return false;
-    }
+       var flag = true;
+   
     // Enfermaria - Informação passada por regExp  *
     var enfermaria = $( inEnfermaria ).val();
     if ( !enfermeiraRegexp.test( enfermaria ) ) {
@@ -174,14 +180,28 @@ define([ "text!../html/ficha/ficha.html" ], function( html ) {
       return false;
     }
     // Nome do paciente - Informação passada por regExp  *
+      
+     
+      
+   
     var paciente = $( inPaciente ).val();
-    if ( !pacienteRegexp.test( paciente ) ) {
-      return false;
+      
+    if ( !pacienteRegexp.test( paciente ) && $( inPaciente ).val() != '' ) {
+
+        
+      $( inPaciente ).css("color", "red");
+          
+      flag = false;
+        
+    } else {
+        
+         $( inPaciente ).css("color", "black"); 
     }
     // Funcionario - Nome do jogador
     var nome = Storage.getLoadedSlot().name.toLowerCase();
     var funcionario = $( inFuncionario ).val().toLowerCase();
     if ( nome !== funcionario ) {
+        
       return false;
     }
     // soro
@@ -192,74 +212,70 @@ define([ "text!../html/ficha/ficha.html" ], function( html ) {
         var horas = parseInt(inicio[ 0 ]);
         var minutos = parseInt(inicio[ 1 ]);
         
-  //    if ( inicio[ 0 ] != horas ) {
-   //     return false;
-   //   }
-        
- //       if( (inicio[ 1 ] != minutos) && ((inicio[ 1 ] + 2) != minutos) && ((inicio[ 1 ] + 1) != minutos) && 
- //         ((inicio[ 1 ] - 2) != minutos) && ((inicio[ 1 ] - 1) != minutos) )
-  //       {
-  //      return false;
-  //    }
-        
       // termino - Horario atual + duração
       var termino = $( inTer ).val().split(":");
-        
         var horasTotais = parseInt(termino[0]);
         var minutosTotais = parseInt(termino[1]);
-      
-        if( horasTotais != (horas + duracao) % 24  ){
-            return false;
+     debugger;
+        if( horasTotais != (horas + duracao) || minutosTotais != minutos  ){
+             $( inTer ).css("color", "red");
+             $( inIni ).css("color", "red");
+            flag = false;
+            
+        } else {
+              $( inTer ).css("color", "black");
+             $( inIni ).css("color", "black");
         }
         
-        if( minutosTotais != minutos ){
-            return false;
-        }
-        
-     /* if ( termino[ 0 ] != (horas + duracao) % 24 ) {
-        return false;
-      }*/
-     
-        //  if( (termino[ 1 ] != minutos) && (termino[ 1 ] + 2) != minutos && (termino[ 1 ] + 1) != minutos )
-        //   if( (termino[ 1 ] - 2) != minutos && (termino[ 1 ] - 1) != minutos )
-   //  {
-     //   return false;
-    //  }
+    
         
         
       // volume - Informação passada por regExp  *
       var volume = $( inVolume1 ).val();
       if ( !volumeRegexp.test( volume ) ) {
-        return false;
+         $( inVolume1 ).css("color", "red");
+         $( inVolume2 ).css("color", "red");
+        flag = false;
+          
+      } else {
+          $( inVolume1 ).css("color", "black");
+          $( inVolume2   ).css("color", "black");
       }
       // duracao - Informação passada por regExp  *
       var duracaoIn = $( inDuracao ).val();
       if ( duracao != duracaoIn ) {
-        return false;
+              $( inDuracao ).css("color", "red");
+        flag = false;
+          
+      }else {
+              $( inDuracao ).css("color", "black");
       }
       // gts - Informação passada por regExp  *
       var gotas = $( inGotas ).val();
       if ( !gotasRegexp.test( gotas ) ) {
-        return false;
+          $( inGotas ).css("color", "red");
+        flag = false;
+          
+      }else {
+          $( inGotas ).css("color", "black");
       }
       // gts Aproximado - Informação passada por regExp  *
       var gotasAprox = $( inGotasAprox ).val();
       if ( !gotasAproxRegexp.test( gotasAprox ) ) {
-        return false;
+           $( inGotasAprox ).css("color", "red");
+        flag = false;
+          
+      }else {
+                  $( inGotasAprox ).css("color", "black");
       }
     } else if ( state === "oral") {
       // oral
       // horario - Horario atual
       var horario = $( inHorario ).val().split(":");
         
-  //    if ( horario[ 0 ] != horas ) {
-   //     return false;
-   //   }
-   //   if ( horario[ 1 ] != minutos && (horario[ 1 ] + 1) != minutos && (horario[ 1 ] - 1) != minutos ) {
-   //     return false;
-   //   }
+ 
     }
-    return true;
+    return flag;
   }
 
   // setters
